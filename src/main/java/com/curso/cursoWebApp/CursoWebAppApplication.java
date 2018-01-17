7356d758f45eeffed2d6383f6aa6ac66818a8cd5
Julio Guerrero <julioguerrero1993@gmail.com>
2018-01-17 15:49:58 -0400

    linea base

---
+++ src/main/java/com/curso/cursoWebApp/CursoWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.curso.cursoWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoWebAppApplication.class, args);
+	}
+}
